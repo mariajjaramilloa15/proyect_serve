f97f92340ecfa00895a73a1af50da57a7a4d7eeb
Maria Jose Jaramillo A <mariaj.jaramilloa@autonoma.edu.co>
2022-05-01 21:09:35 -0500

    Se creo el Request para crear usuario

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,11 +4,11 @@ const UserRouter = require("./user.router")
 
 function routerApi(app) {
   const router = express.Router();
-  /* Endpoint estático: http://localhost:5000/react/user */
-  app.use("/react/user", router);
-  /* Endpoint estático: http://localhost:5000/react/user/userModel */
+  /* Endpoint estático: http://localhost:5000//api/v1/user */
+  app.use("/api/v1/user", router);
+  /* Endpoint estático: http://localhost:5000/api/v1/user/userModel */
   router.use("/userModel", UserModel);
-  /* Endpoint estático: http://localhost:5000/react/user/userRouter */
+  /* Endpoint estático: http://localhost:5000/api/v1/user/userRouter */
   router.use('/userRouter', UserRouter);
 }
 
