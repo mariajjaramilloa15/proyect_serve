e9a335c2a7eabebc3bbca9517835b058d6897744
Maria Jose Jaramillo A <mariaj.jaramilloa@autonoma.edu.co>
2022-05-20 12:00:45 -0500

    actualizacion

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require ("express");
 const bodyParser = require("body-parser");
-//const cors = require("cors");
+const cors = require("cors");
 
 const app = express();
 const { API_VERSION } = require("./config");
@@ -10,7 +10,7 @@ const userRoutes = require("./src/routes/user.router");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 /*Evitar bloqueo del CORS */
-//app.use(cors());
+app.use(cors());
 
 /*Creación de los endpoins del proyecto */
 app.use(`/api/${API_VERSION}`, userRoutes);
