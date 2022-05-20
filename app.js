2787a14aa755f9b662362ad0506a8444f71513f9
Maria Jose Jaramillo A <mariaj.jaramilloa@autonoma.edu.co>
2022-05-20 11:04:51 -0500

    Se actualizo el codigo

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require ("express");
 const bodyParser = require("body-parser");
+//const cors = require("cors");
 
 const app = express();
 const { API_VERSION } = require("./config");
@@ -8,8 +9,11 @@ const userRoutes = require("./src/routes/user.router");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+/*Evitar bloqueo del CORS */
+//app.use(cors());
 
+/*Creación de los endpoins del proyecto */
 app.use(`/api/${API_VERSION}`, userRoutes);
 
-//Configuracion de los Header HTTP
+/* Configuracion de los Header HTTP*/
 module.exports = app;
