f0ec10a73f7d2a3e34eb60b66456cc947065d2fa
Maria Jose Jaramillo A <mariaj.jaramilloa@autonoma.edu.co>
2022-05-20 12:13:21 -0500

    correcion en codigo

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,43 +1,44 @@
-const bcrypt = require ("bcrypt-nodejs");
-const User = require ("../models/user.model.js");
+const bcrypt = require("bcrypt-nodejs");
+const User = require("../models/user.model");
 
-function signUp(req,res) {
+function signUp(req, res){
     const user = new User();
-    const {email, password, repeatPassword} = req.body;
+    const {email, password, repeatPassword } = req.body;
     user.email = email;
-    // Por default almacenamos el rol y si es un usuario activo o no
     user.role = "admin";
     user.active = true;
 
-    // Si no existe una de las contraseñas
-    if (!password || repeatPassword){
-        res.status(404).send({ message: "Las contraseñas son requeridas" });
-    } else {
-        if (password !== repeatPassword){
-            res.status(404).send({ message: "Las contraseñas NO coninciden" });
-        } else{
-            bcrypt.hash(password, null, null, function (err, hash) {
-                //No funciono la encriptacion
+    //Si no existe alguna de las dos passwords
+    if(!password || !repeatPassword){
+        res.status(404).send({message: "Las contraseñas son obligatorias"});
+    }else {
+        if(password !== repeatPassword){
+            res.status(404).send({message: "las contraseñas no coinciden"});
+        }else{
+            bcrypt.hash(password, null, null, function (err, hash){
+                //No funciono la encriptación
                 if(err){
                     res
                     .status(500)
-                    .send({ message: "Error al encriptar la contraseña." });
-                } else{
+                    .send({message: "Error al encriptar la contraseña."});
+                } else {
                     user.password = hash;
-                    user.save((err, userStored) => {
-                        if (err){
-                            res.status(500).send({ message: "El usuario ya existe" });
-                        } else{
-                            if (!userStored){
-                                res.status(404).send({ message: "Error al crear el usuario." });
-                            } else{
-                                res.status(200).send({ user: userStored });
+                    user.save((err, userStore) => {
+                        if(err){
+                            res.status(500).send({message: "El usuario ya existe."});
+                        } else {
+                            if (!userStore){
+                                res.status(404).send({message: "Error al crear el usuario."});
+                            } else {
+                                res.status(200).send({user: userStore});
                             }
                         }
                     });
                 }
-            });
+            })
         }
     }
 }
+
+
 module.exports = { signUp };
